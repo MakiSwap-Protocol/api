9343ea9508894deecd5cdf570bbfb7b7a7bce033
totop716 <totop716@gmail.com>
2021-10-13 11:34:48 +0800

    update api

--- controllers/spot.controller.js
+++ controllers/spot.controller.js
@@ -47,6 +47,28 @@ class spotController {
             const response = await axios(config);
             res.send(response.data);
 
+            const pairs = await getTopPairs()
+            return createSuccessResponse(
+                pairs.reduce<ReturnShape>((accumulator, pair) => {
+                const id0 = getAddress(pair.token0.id)
+                const id1 = getAddress(pair.token1.id)
+        
+                accumulator[`${id0}_${id1}`] = {
+                    base_id: id0,
+                    base_name: pair.token0.name,
+                    base_symbol: pair.token0.symbol,
+                    quote_id: id1,
+                    quote_name: pair.token1.name,
+                    quote_symbol: pair.token1.symbol,
+                    last_price: pair.price ?? '0',
+                    base_volume: pair.previous24hVolumeToken0.toString(),
+                    quote_volume: pair.previous24hVolumeToken1.toString()
+                }
+        
+                return accumulator
+                }, {})
+            )
+
         } catch (error) {
             res.status(400).send(error);
             return;
@@ -70,7 +92,24 @@ class spotController {
             }
 
             const response = await axios(config);
+
+            if (!event.pathParameters?.pair || !/^0x[0-9a-fA-F]{40}_0x[0-9a-fA-F]{40}$/.test(event.pathParameters.pair)) {
+                return createBadRequestResponse('Invalid pair identifier: must be of format tokenAddress_tokenAddress')
+            }
+            
+            const [tokenA, tokenB] = event.pathParameters?.pair.split('_')
+            let idA, idB
+            ;[idA, idB] = [getAddress(tokenA), getAddress(tokenB)]
+        
+            const [reservesA, reservesB] = await getReserves(idA, idB)
+        
+            const timestamp = new Date().getTime()
             res.send(response.data);
+        
+            return createSuccessResponse({
+                timestamp,
+                ...computeBidsAsks(new BigNumber(reservesA), new BigNumber(reservesB))
+            })
 
         } catch (error) {
             res.status(400).send(error);
@@ -79,27 +118,62 @@ class spotController {
         return;
     }
     async getTrades(req, res, next) {
+        const fullUrl = `${TRADES_URL}`;
+        const headers = {};
+        const details = "";
+        headers['Content-Type'] = 'application/json';
+        headers['X-Api-Signature'] = signature(fullUrl, details);
+
+        const config = {
+            method: "GET",
+            url: fullUrl,
+            headers: headers,
+        }
+
+        const response = await axios(config);
+        if (!event.pathParameters?.pair || !/^0x[0-9a-fA-F]{40}_0x[0-9a-fA-F]{40}$/.test(event.pathParameters.pair)) {
+            return createBadRequestResponse('Invalid pair identifier: must be of format tokenAddress_tokenAddress')
+        }
+    
+        const [tokenA, tokenB] = event.pathParameters.pair.split('_')
+        let idA, idB
         try {
-
-            const timestamp = new Date().getTime();
-            const fullUrl = `${TRADES_URL}`;
-            const headers = {};
-            const details = "";
-            headers['Content-Type'] = 'application/json';
-            headers['X-Api-Signature'] = signature(fullUrl, details);
-
-            const config = {
-                method: "GET",
-                url: fullUrl,
-                headers: headers,
-            }
-
-            const response = await axios(config);
-            res.send(response.data);
-
+            ;[idA, idB] = [getAddress(tokenA), getAddress(tokenB)]
         } catch (error) {
-            res.status(400).send(error);
-            return;
+            return createBadRequestResponse('Invalid pair identifier: both asset identifiers must be *checksummed* addresses')
+        }
+    
+        try {
+            const swaps = await getSwaps(idA, idB)
+        
+            return createSuccessResponse(
+                swaps.map(swap => {
+                const aIn = swap.amountAIn !== '0'
+                const aOut = swap.amountAOut !== '0'
+                const bIn = swap.amountBIn !== '0'
+                const bOut = swap.amountBOut !== '0'
+        
+                // a is the base so if the pair sends a and not b then it's a 'buy'
+                const isBuy = aOut && bIn && !aIn && !bOut
+                const isSell = !aOut && !bIn && aIn && bOut
+                const isBorrowBoth = aOut && bOut && aIn && bIn
+        
+                const type = isBuy ? 'buy' : isSell ? 'sell' : isBorrowBoth ? 'borrow-both' : '???'
+                const baseAmount = aOut ? swap.amountAOut : swap.amountAIn
+                const quoteAmount = bOut ? swap.amountBOut : swap.amountBIn
+                return {
+                    trade_id: swap.id,
+                    base_volume: baseAmount,
+                    quote_volume: quoteAmount,
+                    type,
+                    trade_timestamp: swap.timestamp,
+                    price:
+                    baseAmount !== '0' ? new BigNumber(quoteAmount).dividedBy(new BigNumber(baseAmount)).toString() : undefined
+                }
+                })
+            )
+        } catch (error) {
+            return createServerErrorResponse(error)
         }
         return;
     }
--- utils/signature.js
+++
@@ -1,9 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-const signature = (url, data) => {
-    const signData = url + data;
-    const signatureToken = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(signData.toString(CryptoJS.enc.Utf8), process.env.SECKEY));
-    return signatureToken;
-}
-
-module.exports = { signature }
