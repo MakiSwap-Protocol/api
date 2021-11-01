44bf6d923998e5fee7e2f00751c7158b253fcb68
totop716 <totop716@gmail.com>
2021-11-01 11:37:49 +0800

    update config

--- src/v1/apollo/client.ts
+++ src/v1/apollo/client.ts
@@ -1,12 +1,12 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import fetch from 'node-fetch'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import fetch from "node-fetch";
 
 export default new ApolloClient({
   link: new HttpLink({
     fetch,
-    uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
+    uri: "https://n10.hg.network/subgraphs/name/makiexchange/heco",
   }),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
