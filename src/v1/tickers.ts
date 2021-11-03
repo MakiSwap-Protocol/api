bb6eef8f3dccef52f6b2301532b996e06856ae68
totop716 <totop716@gmail.com>
2021-11-03 17:17:43 +0800

    update api

--- src/v1/tickers.ts
+++ src/v1/tickers.ts
@@ -1,29 +1,32 @@
-import { getAddress } from '@ethersproject/address'
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import { createSuccessResponse, createServerErrorResponse } from '../utils/response'
-import { getTopPairs } from './_shared'
+import { getAddress } from "@ethersproject/address";
+import { APIGatewayProxyHandler } from "aws-lambda";
+import {
+  createSuccessResponse,
+  createServerErrorResponse,
+} from "../utils/response";
+import { getTopPairs } from "./_shared";
 
 interface ReturnShape {
   [tokenIds: string]: {
-    base_name: string
-    base_symbol: string
-    base_id: string
-    quote_name: string
-    quote_symbol: string
-    quote_id: string
-    last_price: string
-    base_volume: string
-    quote_volume: string
-  }
+    base_name: string;
+    base_symbol: string;
+    base_id: string;
+    quote_name: string;
+    quote_symbol: string;
+    quote_id: string;
+    last_price: string;
+    base_volume: string;
+    quote_volume: string;
+  };
 }
 
 export const handler: APIGatewayProxyHandler = async () => {
   try {
-    const pairs = await getTopPairs()
+    const pairs = await getTopPairs();
     return createSuccessResponse(
       pairs.reduce<ReturnShape>((accumulator, pair): ReturnShape => {
-        const id0 = getAddress(pair.token0.id)
-        const id1 = getAddress(pair.token1.id)
+        const id0 = getAddress(pair.token0.id);
+        const id1 = getAddress(pair.token1.id);
 
         accumulator[`${id0}_${id1}`] = {
           base_id: id0,
@@ -32,15 +35,15 @@ export const handler: APIGatewayProxyHandler = async () => {
           quote_id: id1,
           quote_name: pair.token1.name,
           quote_symbol: pair.token1.symbol,
-          last_price: pair.price ?? '0',
+          last_price: pair.price ?? "0",
           base_volume: pair.previous24hVolumeToken0.toString(),
-          quote_volume: pair.previous24hVolumeToken1.toString()
-        }
+          quote_volume: pair.previous24hVolumeToken1.toString(),
+        };
 
-        return accumulator
+        return accumulator;
       }, {})
-    )
+    );
   } catch (error) {
-    return createServerErrorResponse(error)
+    return createServerErrorResponse(error);
   }
-}
+};
